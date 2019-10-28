7c6b7735d0e8cb3f1ad9e70036181061e74f6c88
Lim Swee Tat <limsweetat@spdigital.sg>
2019-10-28 23:03:08 +0800

    Actually validate and save the url

--- src/main/java/com/stlim/shortener/rest/ShortenerRestController.java
+++ src/main/java/com/stlim/shortener/rest/ShortenerRestController.java
@@ -1,18 +1,32 @@
 package com.stlim.shortener.rest;
 
 import com.stlim.shortener.models.UrlShortener;
+import com.stlim.shortener.service.UrlShortenerService;
+import org.apache.commons.validator.ValidatorException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class ShortenerRestController {
+	@Autowired
+	UrlShortenerService urlShortenerService;
 
 	@PostMapping("/shortener")
 	public UrlShortener createUrlShortener(
-		@RequestParam(value = "url") String url
+		@RequestBody String url
 	) {
-		return null;
+		UrlShortener result;
+		try {
+			result = urlShortenerService.validateAndSave(url);
+		} catch (ValidatorException ex) {
+			throw new ResponseStatusException(
+				HttpStatus.NOT_ACCEPTABLE, "Invalid.Url", ex);
+		}
+		return result;
 	}
 
 
