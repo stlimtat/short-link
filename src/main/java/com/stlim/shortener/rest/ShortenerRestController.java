40613ee37eb5c9d1f723d19a36fd6b7c8ec0d02b
Lim Swee Tat <limsweetat@spdigital.sg>
2019-10-31 13:09:50 +0800

    Changing the signature of the createUrl api to allow for reading with urlinputform

--- src/main/java/com/stlim/shortener/rest/ShortenerRestController.java
+++ src/main/java/com/stlim/shortener/rest/ShortenerRestController.java
@@ -1,31 +1,35 @@
 package com.stlim.shortener.rest;
 
+import com.stlim.shortener.models.UrlInputForm;
 import com.stlim.shortener.models.UrlShortener;
 import com.stlim.shortener.service.UrlShortenerService;
 import org.apache.commons.validator.ValidatorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.IOException;
 
 @RestController
+@Validated
 public class ShortenerRestController {
 	@Autowired
 	UrlShortenerService urlShortenerService;
 
 	@PostMapping("/shortener")
 	public UrlShortener createUrlShortener(
-		@RequestBody String url
-	) {
+		@Valid @RequestBody UrlInputForm urlInputForm
+		) {
 		UrlShortener result;
 		try {
-			result = urlShortenerService.validateAndSave(url);
+			result = urlShortenerService.validateAndSave(urlInputForm.getUrl());
 		} catch (ValidatorException ex) {
 			throw new ResponseStatusException(
-				HttpStatus.NOT_ACCEPTABLE, "Invalid.Url.(" + url + ")", ex);
+				HttpStatus.NOT_ACCEPTABLE, "Invalid.Url.(" + urlInputForm.getUrl() + ")", ex);
 		}
 		return result;
 	}
