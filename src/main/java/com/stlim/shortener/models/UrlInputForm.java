38c3dbfb1b8ba12ae741229093c3e7b5228889cf
Lim Swee Tat <limsweetat@spdigital.sg>
2019-11-05 14:28:50 +0800

    Adding a short-link to the urlinputform

--- src/main/java/com/stlim/shortener/models/UrlInputForm.java
+++ src/main/java/com/stlim/shortener/models/UrlInputForm.java
@@ -1,10 +1,21 @@
 package com.stlim.shortener.models;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 public class UrlInputForm {
-	@NotEmpty(message="inputForm.notEmpty")
+	@NotEmpty(message="inputForm.url.notEmpty")
 	private String url;
+
+	@Pattern(regexp = "^(|[0-9A-Za-z]{6})$", message = "inputForm.shortLink.incorrectPattern")
+	private String shortLink;
+
+	public UrlInputForm() { }
+
+	public UrlInputForm(String url, String shortLink) {
+		this.url = url;
+		this.shortLink = shortLink;
+	}
 
 	public String getUrl() {
 		return url;
@@ -12,5 +23,13 @@ public class UrlInputForm {
 
 	public void setUrl(String url) {
 		this.url = url;
+	}
+
+	public String getShortLink() {
+		return shortLink;
+	}
+
+	public void setShortLink(String shortLink) {
+		this.shortLink = shortLink;
 	}
 }
